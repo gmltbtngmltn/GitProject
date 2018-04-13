48e440b14d9f7985440bc126f6cd409c89f6730c
gmltbtngmltn <tndlfemd12@naver.com>
2018-04-13 13:50:43 +0900

    버튼 추가

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -2,6 +2,7 @@ package edu.android.gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
     }
 
 }
